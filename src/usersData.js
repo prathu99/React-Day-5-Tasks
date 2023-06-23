d49f142f17492404737017cbbbcc4cf1b0e3b3f4
prathu99 <79712844+prathu99@users.noreply.github.com>
2023-06-23 15:56:58 +0530

    Add files via upload

---
+++ src/usersData.js
@@ -0,0 +1,13 @@
+const users = [{
+    id: "1",
+    name: "Rajendran",
+    email: "rajendrankr25@gmail.com",
+    phone: "9176227713"
+},
+{
+    id: "2",
+    name: "Selvakumari",
+    email: "selvakumari@gmail.com",
+    phone: "1234567890"
+}
+];
